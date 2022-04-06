257ff4059aa3d2580aa735850e258dcb85c69b0c
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-06 17:47:56 +0700

    Тестирование работы модели представления главного окна

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region WindowTitle
+
+        private string _title = "CV-19 statistics analysis";
+
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+
+            //set
+            //{
+            //    //if (Equals(_title, value))
+            //    //{
+            //    //    return;
+            //    //}
+
+            //    //_title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _title, value);
+            //}
+            set => Set(ref _title, value);
+        }
+
+        #endregion
+
+
+    }
+}
