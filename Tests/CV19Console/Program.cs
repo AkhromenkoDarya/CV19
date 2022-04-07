7352ff6ddb3fb57c04ae74c0dd114c552ee520cb
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-07 13:57:13 +0700

    Запрос к удаленному серверу для получения "сырых" данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        private const string _dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/"
+            + @"COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/" 
+            + @"time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var client = new WebClient();
+
+            var client = new HttpClient();
+
+            HttpResponseMessage responseMessage = client.GetAsync(_dataUrl).Result;
+            string csvString = responseMessage.Content.ReadAsStringAsync().Result;
+
+            // Console.WriteLine(csvString);
         }
     }
 }
