13e5db46c1dbcfa7304b838e36c238e762134f90
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-16 23:04:02 +0700

    Реализация фильтрации групп студентов в разметке и кода файлов обработчиков событий окна (code-behind)

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using CV19.Models.Deanery;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CV19
 {
@@ -7,6 +11,46 @@ namespace CV19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group))
+            {
+                return;
+            }
+
+            if (group.Name is null)
+            {
+                return;
+            }
+
+            string filterText = GroupNameFilterTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(filterText))
+            {
+                return;
+            }
+
+            if (group.Name.Contains(filterText, StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            if (group.Description != null && group.Description.Contains(filterText, 
+                StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            e.Accepted = false;
+        }
+
+        private void GroupNameFilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var collection = (CollectionViewSource)textBox.FindResource("GroupCollection");
+            collection.View.Refresh();
         }
     }
 }
