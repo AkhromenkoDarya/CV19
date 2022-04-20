8d53469afb3113127ff650d7d032205eacaaf6b1
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-20 22:28:41 +0700

    Базовая логика модели представления статистики

--- CV19/ViewModels/CountryStatisticsViewModel.cs
+++ CV19/ViewModels/CountryStatisticsViewModel.cs
@@ -1,7 +1,9 @@
-﻿using CV19.ViewModels.Base;
-using System;
+﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.Services;
+using CV19.ViewModels.Base;
 using System.Collections.Generic;
-using System.Text;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
@@ -9,9 +11,54 @@ namespace CV19.ViewModels
     {
         private MainWindowViewModel MainWindowViewModel { get; }
 
+        #region Countries : IEnumerable<CountryInfo> - Статистика по странам
+
+        /// <summary>
+        /// Статистика по странам.
+        /// </summary>
+        private IEnumerable<CountryInfo> _countries;
+
+        /// <summary>
+        /// Статистика по странам.
+        /// </summary>
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _countries;
+
+            private set => Set(ref _countries, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region RefreshDataCommand
+
+        public ICommand RefreshDataCommand { get; }
+
+        //private bool CanRefreshDataCommandExecute(object p)
+        //{
+        //    return true;
+        //}
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = DataService.GetData();
+        }
+
+        #endregion
+
+        #endregion
+
         public CountryStatisticsViewModel(MainWindowViewModel mainWindowViewModel)
         {
             MainWindowViewModel = mainWindowViewModel;
+
+            #region Команды
+
+            RefreshDataCommand = new RelayCommand(OnRefreshDataCommandExecuted);
+
+            #endregion
         }
     }
 }
