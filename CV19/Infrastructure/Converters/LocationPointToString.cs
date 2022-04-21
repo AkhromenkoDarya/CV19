232d79edb8ce0d01679573c6821604351b3f3253
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-21 18:25:07 +0700

    Добавлены конвертеры

---
+++ CV19/Infrastructure/Converters/LocationPointToString.cs
@@ -0,0 +1,39 @@
+﻿using CV19.Infrastructure.Converters.Base;
+using System;
+using System.Globalization;
+using System.Windows;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class LocationPointToString : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (!(value is Point point))
+            {
+                return null;
+            }
+
+            return $"Lat: {point.X}; Lon: {point.Y}";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (!(value is string str))
+            {
+                return null;
+            }
+
+            var components = str.Split(';');
+            var latitudeString = components[0].Split(':')[1];
+            var longitudeString = components[1].Split(':')[1];
+
+            var latitude = double.Parse(latitudeString);
+            var longitude = double.Parse(longitudeString);
+
+            return new Point(latitude, longitude);
+        }
+    }
+}
---
+++ CV19/Infrastructure/Converters/Ratio.cs
@@ -0,0 +1,42 @@
+﻿using CV19.Infrastructure.Converters.Base;
+using System;
+using System.Globalization;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class Ratio : Converter
+    {
+        public double Coefficient { get; set; } = 1;
+
+        public override object Convert(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return null;
+            }
+
+            var x = System.Convert.ToDouble(value, culture);
+
+            return x * Coefficient;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return null;
+            }
+
+            var x = System.Convert.ToDouble(value, culture);
+
+            return x / Coefficient;
+        }
+    }
+}
