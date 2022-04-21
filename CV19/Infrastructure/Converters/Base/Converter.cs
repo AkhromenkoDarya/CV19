12721cacb17e3e65ca33842cea4332c7e0bbf100
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-21 15:43:27 +0700

    Реализован базовый класс конвертера

---
+++ CV19/Infrastructure/Converters/Base/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters.Base
+{
+    internal abstract class Converter : IValueConverter
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, 
+            CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            throw new NotSupportedException("Back conversion is not supported.");
+        }
+    }
+}
