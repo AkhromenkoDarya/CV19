1ae39bd21a936d2d968565c1a27c7bb1c015e338
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-22 18:47:28 +0700

    Индикация карты

---
+++ CV19/Infrastructure/Converters/PointToMapLocation.cs
@@ -0,0 +1,37 @@
+﻿using CV19.Infrastructure.Converters.Base;
+using MapControl;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Converters
+{
+    [MarkupExtensionReturnType(typeof(PointToMapLocation))]
+    [ValueConversion(typeof(Point), typeof(Location))]
+    internal class PointToMapLocation : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (!(value is Point point))
+            {
+                return null;
+            }
+
+            return new Location(point.X, point.Y);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, 
+            CultureInfo culture)
+        {
+            if (!(value is Location location))
+            {
+                return null;
+            }
+
+            return new Point(location.Latitude, location.Longitude);
+        }
+    }
+}
