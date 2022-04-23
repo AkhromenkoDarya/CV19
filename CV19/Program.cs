94d4f8851f21eb8faa39479a3c856e8966dec5a9
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-23 20:01:57 +0700

    Собственная точка входа в приложение - метод Program.Main()

---
+++ CV19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CV19
+{
+    public static class Program
+    {
+        [STAThreadAttribute()]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
