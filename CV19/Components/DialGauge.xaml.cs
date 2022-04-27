7c82efe0de5788e15988f6219d2c11b7e4e73402
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-27 16:07:42 +0700

    Определение свойства зависимости

--- CV19/Components/DialGauge.xaml.cs
+++ CV19/Components/DialGauge.xaml.cs
@@ -1,26 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CV19.Components
 {
-    /// <summary>
-    /// Interaction logic for DialGauge.xaml
-    /// </summary>
-    public partial class DialGauge : UserControl
+    public partial class DialGauge
     {
-        public DialGauge()
+        #region Value : double - Значение стрелки индикатора
+
+        /// <summary>
+        /// Значение стрелки индикатора.
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty
+            = DependencyProperty.Register(
+                nameof(Value),
+                typeof(double ),
+                typeof(DialGauge),
+                new PropertyMetadata(default(double )));
+
+        //private static void OnPropertyChanged(DependencyObject d, 
+        //    DependencyPropertyChangedEventArgs e)
+        //{
+        //    var dialGauge = (DialGauge)d;
+        //    dialGauge.ArrowRotator.Angle = (double)e.NewValue;
+        //}
+
+        //private static object OnCoerceValue(DependencyObject d, object baseValue)
+        //{
+        //    var value = (double)baseValue;
+        //    return Math.Max(0, Math.Min(100, value));
+        //}
+
+        //private static bool OnValidateValue(object value)
+        //{
+        //    return true;
+        //}
+
+        /// <summary>
+        /// Значение стрелки индикатора.
+        /// </summary>
+        [Description("Значение стрелки индикатора")]
+        [Category("Моя категория")]
+        public double  Value
         {
-            InitializeComponent();
+            get => (double ) GetValue(ValueProperty);
+
+            set => SetValue(ValueProperty, value);
         }
+
+        #endregion
+
+
+        public DialGauge() => InitializeComponent();
     }
 }
