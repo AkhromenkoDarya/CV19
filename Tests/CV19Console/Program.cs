dfdb2023d0519410f55a395ebf9d0cb993162fbb
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-29 11:07:16 +0700

    Блокировка потоков и их взаимная синхронизация

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace CV19Console
@@ -10,14 +9,14 @@ namespace CV19Console
 
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "MainThread";
+            //Thread.CurrentThread.Name = "MainThread";
 
-            var clockThread = new Thread(ThreadMethod)
-            {
-                Name = "ClockThread",
-                IsBackground = true,
-                Priority = ThreadPriority.AboveNormal
-            };
+            //var clockThread = new Thread(ThreadMethod)
+            //{
+            //    Name = "ClockThread",
+            //    IsBackground = true,
+            //    Priority = ThreadPriority.AboveNormal
+            //};
 
             //secondThread.Start(42);
 
@@ -36,58 +35,137 @@ namespace CV19Console
             //    Console.WriteLine(i);
             //}
 
-            var values = new List<int>();
-            var threads = new Thread[10];
+            //var values = new List<int>();
+            //var threads = new Thread[10];
 
-            var lockObject = new object();
+            //var lockObject = new object();
 
-            for (var i = 0; i < threads.Length; i++)
-            {
-                threads[i] = new Thread(() =>
-                {
-                    for (var j = 0; j < 10; j++)
-                    {
-                        //lock(values)
-                        lock (lockObject)
-                            values.Add(Thread.CurrentThread.ManagedThreadId);
-                        Thread.Sleep(1);
-                    }
-                });
-            }
+            //for (var i = 0; i < threads.Length; i++)
+            //{
+            //    threads[i] = new Thread(() =>
+            //    {
+            //        for (var j = 0; j < 10; j++)
+            //        {
+            //            //lock(values)
+            //            lock (lockObject)
+            //                values.Add(Thread.CurrentThread.ManagedThreadId);
+            //            Thread.Sleep(1);
+            //        }
+            //    });
+            //}
 
-            #region LockRealization
+            //#region LockRealization
 
-            Monitor.Enter(lockObject);
+            //Monitor.Enter(lockObject);
 
-            try
-            {
-                // Критическая секция.
-            }
-            finally
-            {
-                Monitor.Exit(lockObject);
-            }
+            //try
+            //{
+            //    // Критическая секция.
+            //}
+            //finally
+            //{
+            //    Monitor.Exit(lockObject);
+            //}
+
+            //#endregion
+
+            //foreach (Thread thread in threads)
+            //{
+            //    thread.Start();
+            //}
+
+            ////clockThread.Join();
+
+            //if (!clockThread.Join(100))
+            //{
+            //    // Прерывает поток в любой точке процесса его выполнения.
+            //    //clockThread.Abort();
+
+            //    // Альтернативный вариант.
+            //    clockThread.Interrupt();
+            //}
+
+            //var mutex = new Mutex();
+
+            //var semaphore = new Semaphore(0, 10);
+            //semaphore.WaitOne();
+
+            //// Действия в критической секции.
+
+            //semaphore.Release();
+
+            #region ManualResetEvent
+
+            //var manualResetEvent = new ManualResetEvent(false);
+            //EventWaitHandle threadGuidance = manualResetEvent;
+
+            //var testThreads = new Thread[10];
+
+            //for (var i = 0; i < testThreads.Length; i++)
+            //{
+            //    int localIndex = i;
+
+            //    testThreads[i] = new Thread(() =>
+            //    {
+            //        Console.WriteLine("Thread Id: {0} - started", Thread.CurrentThread
+            //            .ManagedThreadId);
+
+            //        threadGuidance.WaitOne();
+
+            //        Console.WriteLine("Value {0}", localIndex);
+            //        Console.WriteLine("Thread Id: {0} - terminated", Thread.CurrentThread
+            //            .ManagedThreadId);
+            //    });
+
+            //    testThreads[i].Start();
+            //}
+
+            //Console.WriteLine("Ready to start threads");
+            //Console.ReadLine();
+
+            //threadGuidance.Set();
+            //Console.ReadLine();
 
             #endregion
 
-            foreach (Thread thread in threads)
+            #region AutoResetEvent
+
+            var autoResetEvent = new AutoResetEvent(false);
+            EventWaitHandle threadGuidance = autoResetEvent;
+
+            var testThreads = new Thread[10];
+
+            for (var i = 0; i < testThreads.Length; i++)
             {
-                thread.Start();
+                int localIndex = i;
+
+                testThreads[i] = new Thread(() =>
+                {
+                    Console.WriteLine("Thread Id: {0} - started", Thread.CurrentThread
+                        .ManagedThreadId);
+
+                    threadGuidance.WaitOne();
+
+                    Console.WriteLine("Value {0}", localIndex);
+                    Console.WriteLine("Thread Id: {0} - terminated", Thread.CurrentThread
+                        .ManagedThreadId);
+
+                    threadGuidance.Set();
+                });
+
+                testThreads[i].Start();
             }
 
-            //clockThread.Join();
-
-            if (!clockThread.Join(100))
-            {
-                // Прерывает поток в любой точке процесса его выполнения.
-                //clockThread.Abort();
-
-                // Альтернативный вариант.
-                clockThread.Interrupt();
-            }
-
-            Console.WriteLine(string.Join(",", values));
+            Console.WriteLine("Ready to start threads");
             Console.ReadLine();
+
+            threadGuidance.Set();
+            Console.ReadLine();
+
+            #endregion
+
+            //Console.WriteLine(string.Join(",", values));
+            //Console.ReadLine();
         }
 
         private static void PrintMethod(string message, int count, int timeout)
