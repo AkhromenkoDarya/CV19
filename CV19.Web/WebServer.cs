58960b73f5cb373248f633fac6e9617ca0037ff9
Darya Akhromenko <otulissa2012@yandex.ru>
2022-04-29 15:10:51 +0700

    Заготовка асинхронного сервера

--- CV19.Web/WebServer.cs
+++ CV19.Web/WebServer.cs
@@ -1,10 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Net;
 
 namespace CV19.Web
 {
     public class WebServer
     {
+        //private TcpListener _listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
+
+        private HttpListener _listener;
+
+        private readonly int _port;
+
+        private bool _isEnabled;
+
+        private readonly object _syncRoot = new object();
+
+        public int Port => _port;
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+
+            set
+            {
+                if (value)
+                {
+                    Start();
+                    return;
+                }
+                
+                Stop();
+            }
+        }
+
+        public WebServer(int port) => _port = port;
+
+        public void Start()
+        {
+            if (IsEnabled)
+            {
+                return;
+            }
+
+            //lock(this)
+            lock (_syncRoot)
+            {
+                if (IsEnabled)
+                {
+                    return;
+                }
+
+                _listener = new HttpListener();
+                _listener.Prefixes.Add($"http://*:{_port}");
+                _listener.Prefixes.Add($"http://+:{_port}");
+                IsEnabled = true;
+            }
+
+            Listen();
+        }
+
+        public void Stop()
+        {
+            if (!IsEnabled)
+            {
+                return;
+            }
+            
+            //lock(this)
+            lock (_syncRoot)
+            {
+                if (!IsEnabled)
+                {
+                    return;
+                }
+
+                _listener = null;
+                IsEnabled = false;
+            }
+        }
+
+        private void Listen()
+        {
+
+        }
     }
 }
