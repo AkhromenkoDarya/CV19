224bda5a41d3f60eef6926be0336c321f26cad63
Darya Akhromenko <otulissa2012@yandex.ru>
2022-05-05 11:08:01 +0700

    Макет представления окна управления студентами

--- CV19/ViewModels/StudentManagementViewModel.cs
+++ CV19/ViewModels/StudentManagementViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace CV19.ViewModels
-{
-    class StudentManagementViewModel
-    {
+﻿using CV19.ViewModels.Base;
 
+namespace CV19.ViewModels
+{
+    internal class StudentManagementViewModel : ViewModel
+    {
+        #region Title : string - Заголовок окна
+
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        private string _title = "Student Management";
+
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+
+            set => Set(ref _title, value);
+        }
+
+        #endregion
     }
 }
